ef339602a1e8f394c57a0e039807353791bf69bf
Noah Snelson <noah.snelson@protonmail.com>
2021-09-13 21:55:53 -0700

    Allow users to submit form for shortened links

--- src/clj/drosera/routes/links.clj
+++ src/clj/drosera/routes/links.clj
@@ -3,19 +3,23 @@
    [drosera.db.core :as db]
    [drosera.links :as links]
    [drosera.middleware :as middleware]
-   [ring.util.http-response :as response]))
+   [ring.util.http-response :refer [content-type ok]]))
 
 (defn redirect-slug
   [{{:keys [slug]} :path-params}]
   (let [link-id (links/decode-slug-to-id slug)
         link-row (db/get-link-by-id {:id link-id})]
-    (if link-row
-      (response/found {:link link-row})
-      (response/not-found "This link has expired."))))
+    (content-type (ok
+                   (if link-row
+                     (str "Redirecting to " (:link link-row))
+                     "Link has expired."))
+                  "text/plain")))
 
 (defn create-link
-  [{{:keys [link]} :body-params}]
-  (response/ok :headers {"Content-Type" "text/html"} :body link))
+  [{{:keys [link]} :params}]
+  (let [{id :id} (db/create-link! {:link link :user_id nil})
+        slug (links/encode-id-to-slug id)]
+  {:status 200 :headers {"Content-Type" "text/html"} :body slug}))
 
 (defn links-routes
   []
@@ -23,5 +27,5 @@
   ""
    {:middleware [middleware/wrap-csrf
                  middleware/wrap-formats]}
-   ["/link/create" {:get create-link}]
+   ["/link/create" {:post create-link}]
    ["/:slug" {:get redirect-slug}]])
