188e3c1437ffd469d2605115d2bc177266235f98
Noah Snelson <noah.snelson@protonmail.com>
2021-10-09 22:18:50 -0700

    Add fallback 404 route for missing pages

--- src/sundews/routes.clj
+++ src/sundews/routes.clj
@@ -1,5 +1,6 @@
 (ns sundews.routes
   (:require [compojure.core :as compojure]
+            [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
             [sundews.handlers :refer [index-handler create-link-handler redirect-handler]]
             [sundews.db :as db]))
@@ -7,7 +8,8 @@
 (compojure/defroutes routes
   (compojure/POST "/links/create" [link] (create-link-handler db/db link))
   (compojure/GET "/:slug" [slug] (redirect-handler db/db slug))
-  (compojure/GET "/" [] (index-handler)))
+  (compojure/GET "/" [] (index-handler))
+  (route/not-found "<h1>Page not found.</h1>"))
 
 (def handler
   (wrap-defaults routes site-defaults))
