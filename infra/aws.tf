d717b236be5ab834f056bcb1815f2436da9796e1
Noah Snelson <noah.snelson@protonmail.com>
2021-11-11 21:34:14 -0800

    Start managing infrastructure with Terraform

---
+++ infra/aws.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-west-1"
+}
+
+resource "aws_instance" "web_server" {
+  ami           = "ami-0c8b7aacccd254467" // ECS-optimized Amazon Linux 2
+  instance_type = "t2.micro"
+}
