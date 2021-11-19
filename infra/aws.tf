e1f6b1c550772f6470d1d56ae482e21d9bce2aa7
Noah Snelson <noah.snelson@protonmail.com>
2021-11-18 23:03:54 -0800

    Add terraform modules for ECR and GitHub IAM user

--- infra/aws.tf
+++ infra/aws.tf
@@ -14,7 +14,24 @@ provider "aws" {
   region  = "us-west-1"
 }
 
-resource "aws_instance" "web_server" {
+resource "aws_instance" "sundews_web_server_1" {
   ami           = "ami-0c8b7aacccd254467" // ECS-optimized Amazon Linux 2
   instance_type = "t2.micro"
+}
+
+resource "aws_ecr_repository" "sundews_container_repository" {
+  name                 = "sundews_container_repo"
+  image_tag_mutability = "IMMUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_iam_user" "sundews_github_actions_ci" {
+  name = "sundews_github_actions"
+}
+
+resource "aws_iam_access_key" "sundews_github_actions_ci" {
+  user = aws_iam_user.sundews_github_actions_ci.name
 }
